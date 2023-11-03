623c71b8a7bfa5abed517e7aae298bfaeaa511ca
Pablo Garcia Urzua <142696188+PabloGarciaU@users.noreply.github.com>
2023-11-03 02:58:18 -0300

    Tutorial plantilla completado
    agradecido del gringo de este canal: https://www.youtube.com/watch?v=Qf0wri9MvMY&t=2s explica todo de pana y se resolvio el problema de la documentacion, ahora hay aplicar los requerimientos del trabajo y despues el login para finalmente documentar

--- app/api_models.py
+++ app/api_models.py
@@ -14,7 +14,12 @@ course_model = api.model("Course", {
     "students": fields.List(fields.Nested(student_model))
 })
 
+course_imput_model = api.model("CourseInput", {
+    "name": fields.String(required=True)
+})
 
-
-
+student_input_model = api.model("StudentInput", {
+    "name": fields.String(required=True),
+    "course_id": fields.Integer(required=True)
+})
 
--- app/resources.py
+++ app/resources.py
@@ -1,6 +1,7 @@
 from flask_restx import Resource, Namespace
 
-from .api_models import course_model, student_model
+from .api_models import course_model, student_model, course_imput_model, student_input_model
+from .extensions import db
 from .models import Course, Student
 
 ns = Namespace("api")
@@ -16,8 +17,46 @@ class CoursesAPI(Resource):
     def get(self):
         return Course.query.all()
     
+    @ns.expect(course_imput_model)
+    @ns.marshal_with(course_model)
+    def post(self):
+        print(ns.payload)
+        course = Course(name=ns.payload["name"])
+        db.session.add(course)
+        db.session.commit()
+        return course, 201
+    
+@ns.route("/courses/<int:id>")
+class CourseAPI(Resource):
+    @ns.marshal_with(course_model)
+    def get(self, id):
+        return Course.query.get_or_404(id)
+    
+    def delete(self, id):
+        course = Course.query.get_or_404(id)
+        db.session.delete(course)
+        db.session.commit()
+        return "", 204
+    
+    @ns.expect(course_imput_model)
+    @ns.marshal_with(course_model)
+    def put(self, id):
+        course = Course.query.get_or_404(id)
+        course.name = ns.payload["name"]
+        db.session.add(course)
+        db.session.commit()
+        return course, 200
+
 @ns.route("/students")
 class StudentsAPI(Resource):
     @ns.marshal_list_with(student_model)
     def get(self):
         return Student.query.all()
+    
+    @ns.expect(student_input_model)
+    @ns.marshal_with(student_model)
+    def post(self):
+        student = Student(name=ns.payload["name"], course_id=ns.payload["course_id"])
+        db.session.add(student)
+        db.session.commit()
+        return student, 201
